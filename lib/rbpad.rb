056f62b7abcb48ada76fb69cddee51a2a73cde72
kojix2 <2xijok@gmail.com>
2021-05-01 17:19:35 +0900

    Allow rbpad to be called as a library

--- lib/rbpad.rb
+++ lib/rbpad.rb
@@ -11,22 +11,3 @@ require 'optparse'
 require 'rbpad/pad'
 
 Encoding.default_external = 'UTF-8'
-
-opts = ARGV.getopts('ex')
-
-lang = (opts['e'] ? :en : :jp)           # :jp or :en
-
-if opts['x']
-  pad = Rbpad::Pad.new(lang, 0, 0)       # maximized
-else
-  if ARGV.size >= 2
-    w = ARGV[0].to_i
-    h = ARGV[1].to_i
-    pad = Rbpad::Pad.new(lang, w, h)     # specified
-  else
-    pad = Rbpad::Pad.new(lang)           # default
-  end
-end
-
-Gtk.main
-
